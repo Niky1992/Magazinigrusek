d7c3878a7cc4b3fba572d2c1567e1b85dcdd5f3b
Niky1992 <138252944+Niky1992@users.noreply.github.com>
2023-12-20 21:51:08 +0300

    програма

---
+++ toys/Toy.java
@@ -0,0 +1,16 @@
+package toys;
+
+public class Toy {
+
+    public int getId() {
+        return 0;
+    }
+
+    public void setWeight(double newWeight) {
+    }
+
+    public double getWeight() {
+        return 0;
+    }
+
+}
---
+++ toys/ToyRaffle.java
@@ -0,0 +1,37 @@
+package toys;
+
+import java.util.PriorityQueue;
+
+
+public class ToyRaffle {
+
+    private final String[] toyIds;       // Массив идентификаторов игрушек
+    private final String[] toyNames;     // Массив названий игрушек
+    private final int[] toyWeights;      // Массив весов (частоты выпадения) игрушек
+    private final PriorityQueue<String> raffleQueue; // Очередь с игрушками для розыгрыша
+
+    
+    public ToyRaffle(String[] toyIds, String[] toyNames, int[] toyWeights) {
+        this.toyIds = toyIds;
+        this.toyNames = toyNames;
+        this.toyWeights = toyWeights;
+        this.raffleQueue = new PriorityQueue<>();
+
+        
+        for (int i = 0; i < toyIds.length; i++) {
+            int weight = toyWeights[i];
+            for (int j = 0; j < weight; j++) {
+                raffleQueue.add(toyIds[i]);
+            }
+        }
+    }
+
+    
+    public String drawToy() {
+        if (raffleQueue.isEmpty()) {
+            return "Розыгрыш завершен. Все игрушки разыграны.";
+        }
+        return raffleQueue.poll();
+    }
+}
+
